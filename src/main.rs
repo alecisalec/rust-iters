24b2cf405bf70dbdb4fa35c2f852d3251a3358af
Alec Graham <alec.p.graham@protonmail.com>
2025-05-21 13:34:01 -0400

    vect slices

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,28 @@ fn print_elements_slice(el : &[String]){
 }
 
 
+fn shorten_strings(el : &mut [String]){
+
+    el.iter_mut().for_each(|el| {
+        el.truncate(1);
+    });
+
+    println!("{:#?}", el)
+}
+
 
 
 fn main() {
     let colors = vec![ String::from("red"), String::from("blue"),String::from("green")];
-
+    let mut colors2 = vec![ String::from("red"), String::from("blue"),String::from("green")];
 
     //let mut colors_iter = colors.iter(); 
 
     print_elements(&colors);
     print_elements_slice(&colors[1..3]); // slice
     print_elements_slice(&colors); // still full vector
+
+    shorten_strings(&mut colors2);
 
 
 }
